fa938f86ea6de90e7bf2456a6e9f4494a75cd07b
Horatiu Cheval <horatiucheval@gmail.com>
2023-10-26 10:38:34 +0300

    Lab2 bonus

--- Lab2.lean
+++ Lab2.lean
@@ -4,11 +4,12 @@
 open Classical
 
 /-
-  `Prop` is the type of propositions. 
-  Examples of propositions are equalities, like the ones we've seen in Lab1. 
+  `Prop` is the type of propositions.
+  Examples of propositions are equalities, like the ones we've seen in Lab1.
 -/
-#check Prop 
+#check Prop
 #check 5 = 3
+#check (rfl : 5 = 5)
 
 /-
   A proposition is itself a type. If `p : Prop`, we can speak of terms `h` of type `p`.
@@ -22,7 +23,7 @@ open Classical
   Each of them is governed by so-called principles of *introduction* and *elimination*.
   The introduction principle answers the question:
   *how can one, in general, prove a conjunction / disjunction?*,
-  while the elimination principle refers to 
+  while the elimination principle refers to
   *how can one prove something from a conjunction / disjunction?*
 -/
 
@@ -34,36 +35,37 @@ open Classical
 variable (p q : Prop)
 
 /-
-  ## And 
-  The notation `p ∧ q` is used for `And p q`. 
+  ## And
+  The notation `p ∧ q` is used for `And p q`.
 -/
-#print And 
+#check And
+#print And
 #check And p q
-#check @And.intro 
-#check @And.left 
-#check @And.right 
+#check @And.intro
+#check @And.left
+#check @And.right
 
 /-
   ## Or
   The notation `p ∨ q` is used ofr `Or p q`.
 -/
-#check Or 
+#check Or
 #check Or p q
-#check @Or.inl 
-#check @Or.inr 
+#check @Or.inl
+#check @Or.inr
 #check @Or.elim
 
 /-
   #False
 -/
-#print False 
+#print False
 #check @False.elim
 
 /-
   ## Not
   Negation is defined by `Not p := p → False`.
 -/
-#check Not 
+#check Not
 #check Not p
 
 #check em
@@ -72,7 +74,8 @@ variable (p q : Prop)
   Exercise 1: Prove the following theorem.
   Hint: Look at the `applyFunction` function defined in Lab1
 -/
-theorem modus_ponens : p → (p → q) → q := sorry
+theorem modus_ponens : p → (p → q) → q :=
+  fun hp hpq => hpq _
 
 /-
   Exercise 2: Prove the following theorem.
@@ -82,7 +85,7 @@ theorem and_comm : p ∧ q → q ∧ p := sorry
 
 
 /-
-  In principle, any theorem can be proved by simply writing a function of the appropriate type 
+  In principle, any theorem can be proved by simply writing a function of the appropriate type
   (the type of the theorem's statement), like above.
   This can get unwieldy for complex proofs, so Lean offers a different embedded language called *tactic mode*.
   At any point in a proof, there is a *proof state* composed of a number of hypotheses and a number of goals needing to be proved.
@@ -94,32 +97,32 @@ theorem modus_ponens_tactics : p → (p → q) → q := by --we enter tactic mod
   intros hp -- suppose a proof of `p → q` exists, and call it `h_imp_q`
             -- note the change in the proof state
   -- we still have an implication to prove, so we again assume its premise.
-  intros hpq 
+  intros hpq
   -- we need to prove `q`. We can obtain `q` from the conclusion of `hpq` if we provide the right premise to it
   apply hpq -- the goal would follow from `hpq` if we proved its required conclusion. Note the goal change
-  -- the goal is now just an assumption 
+  -- the goal is now just an assumption
   assumption
 
 theorem and_comm_tactics : p ∧ q → q ∧ p := by --we enter tactic mode with `by`. Note the infoview on the right.
-  -- we need to prove an implication. We first suppose its premise 
+  -- we need to prove an implication. We first suppose its premise
   intros hpq -- suppose a proof of `p wedge q` exists, and call it `hpq`
-             -- note the change in the proof state 
-  -- we know p ∧ q, and from it can obtain both `p` and `q` by 
-  cases hpq with | intro hp hq => 
-  -- we need to prove `q ∧ p`. We know this can be proved from `And.intro` 
-  apply And.intro 
+             -- note the change in the proof state
+  -- we know p ∧ q, and from it can obtain both `p` and `q` by
+  cases hpq with | intro hp hq =>
+  -- we need to prove `q ∧ p`. We know this can be proved from `And.intro`
+  apply And.intro
   -- in order to apply `And.intro` we need to to have both a proof of `p` and a proof of `q`
   -- Lean produced two new goals, both of which are trivial two solve
-  case left => assumption 
-  case right => assumption 
-  
+  case left => assumption
+  case right => assumption
+
 
 /-
   Usually, tactic mode and term mode may be freely combined.
   For instance, a more concise version of the above may be:
 -/
-theorem and_comm_tactics' : p ∧ q → q ∧ p := by 
-  intros hpq 
+theorem and_comm_tactics' : p ∧ q → q ∧ p := by
+  intros hpq
   cases hpq with | intro hp hq =>
   exact And.intro hq hp
 
@@ -132,47 +135,57 @@ example : p → q → (p ∧ q) := sorry
 /-
   Exercise 4: Give the shortest possible *term mode* proof you can think of for the above statement
 -/
-example : p → q → (p ∧ q) := sorry 
+example : p → q → (p ∧ q) := sorry
 
 
 /-
-  Disjunction can be eliminated by pattern matching. 
+  Disjunction can be eliminated by pattern matching.
   A proof of `p ∨ q` is, by definition, either a proof of `p` or a proof `q`.
-  Dually, in order to prove `p ∨ q`, one chooses only one of the disjuncts and provides a proof for it. 
+  Dually, in order to prove `p ∨ q`, one chooses only one of the disjuncts and provides a proof for it.
 -/
-example : p ∨ q → q ∨ p := 
-  fun h => match h with 
-  | Or.inl hp => Or.inr hp 
-  | Or.inr hq => Or.inl hq 
+example : p ∨ q → q ∨ p :=
+  fun h => match h with
+  | Or.inl hp => Or.inr hp
+  | Or.inr hq => Or.inl hq
 
-example : p ∨ q → q ∨ p := by 
-  intros h 
-  cases h with 
-  | inl hp => 
-    exact Or.inr hp 
-  | inr hp => 
+example : p ∨ q → q ∨ p := by
+  intros h
+  cases h with
+  | inl hp =>
+    exact Or.inr hp
+  | inr hp =>
     exact Or.inl hp
 
 /-
   Exercise 5: -/
-example : p ∧ q → q ∨ p := sorry 
+example : p ∧ q → q ∨ p := sorry
 
 /- Exercise 6: -/
-example : p → (¬p) → False := sorry 
+example : p → (¬p) → False := sorry
 
 /- Exercise 7: -/
-example : p → (¬p) → q := sorry 
+example : p → (¬p) → q := sorry
 
 /- Exercise 8: -/
-example : p ∧ (¬p) → q := sorry 
+example : p ∧ (¬p) → q := sorry
 
 /- Exercise 9: -/
-example : p → ¬¬p := sorry 
+example : p → ¬¬p := sorry
 
 /- Exercise 10: -/
-example : ¬¬p → p := sorry 
+example : ¬¬p → p := sorry
 
+variable (r : Prop)
 
+/- Bonus exercises. Use `Iff.intro` to split the equivalence into two implications. -/
+example : (p ∧ q → r) ↔ (p → q → r) := sorry
 
+example : (p → q) ↔ (¬q → ¬p) := sorry
 
+example : (p → q) ↔ (¬p ∨ q) := sorry
 
+example : ¬(p ∨ q) ↔ ¬p ∧ ¬q := sorry
+
+example : ¬(p ∧ q) ↔ ¬p ∨ ¬q := sorry
+
+example : (((p → q) → p) → p) := sorry
